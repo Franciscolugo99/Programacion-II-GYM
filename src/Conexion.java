5cff3e20882bc97b865b9a50f83ea28b0afd7e84
FranciscoLugo <Franciscolugo613@gmail.com>
2022-09-29 15:23:58 -0300

    actualizo el proyecto

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,10 +1,60 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class Conexion {
 
+    private  Conexion(){
+
+    }
+
+    //creo una variable en la que guardo el estado de la conxion de BD
     private static Connection conn;
 
-    private Conexion instancia;
+    //creo una variable para crear una sola instancia
+    private static Conexion instancia;
+
+
+    private  static final String URL = "jdbc:mysql://localhost:3306/GYM";
+    private  static final String User = "root";
+    private  static final String Pass = "root";
+
+    public Connection conectar () throws SQLException {
+        try {
+            conn =DriverManager.getConnection(URL, User, Pass);
+            System.out.println("Connexion exitosa");
+             return conn;
+        }catch (Exception e){
+            System.out.println("Fallo la Connexion ");
+        }
+
+        return conn;
+    }
+
+    public void cerrarConex(Statement statement, ResultSet query) throws SQLException {
+        try {
+            statement.close();
+            query.close();
+            conn.close();
+
+        }catch (Exception e){
+
+            statement.close();
+            query.close();
+            conn.close();
+
+        } finally {
+            conn.close();
+        }
+
+    }
+
+
+    public static Conexion getInstancia(){
+        if (instancia == null){
+            instancia = new Conexion();
+        }
+            return instancia;
+
+    }
 
 
 
